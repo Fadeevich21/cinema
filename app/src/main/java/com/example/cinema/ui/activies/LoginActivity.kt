49f57e7783ddd535d05813ff968582bdf2ffa24a
fadeevich <fad.danila053@gmail.com>
2023-05-04 16:04:03 +0300

    исправил ошибки при регистрации

--- app/src/main/java/com/example/cinema/ui/activies/LoginActivity.kt
+++ app/src/main/java/com/example/cinema/ui/activies/LoginActivity.kt
@@ -1,13 +1,16 @@
 package com.example.cinema.ui.activies
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cinema.App
 import com.example.cinema.databinding.ActivityLoginBinding
 import com.example.cinema.ui.viewModel.LoginViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -26,6 +29,7 @@ class LoginActivity : AppCompatActivity() {
                 App.user = viewModel.loginUserLive.value
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
+                finish()
             } else {
                 Toast.makeText(applicationContext, "Login not success", Toast.LENGTH_SHORT).show()
             }
@@ -35,7 +39,8 @@ class LoginActivity : AppCompatActivity() {
             if (viewModel.isRegisterLive.value == true)
                 Toast.makeText(applicationContext, "Register success", Toast.LENGTH_SHORT).show()
             else
-                Toast.makeText(applicationContext, "Register not success", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Register not success", Toast.LENGTH_SHORT)
+                    .show()
         }
 
         binding.loginButton.setOnClickListener {
@@ -49,5 +54,38 @@ class LoginActivity : AppCompatActivity() {
             val password = binding.passwordForm.text.toString()
             viewModel.register(username = username, password = password)
         }
+
+        setupFloatingLabelError()
+    }
+
+    private fun setupFloatingLabelError() {
+        val usernameLabel = binding.usernameTextInputLayout
+        usernameLabel.editText!!.addTextChangedListener(object : TextWatcher {
+
+            override fun onTextChanged(text: CharSequence, start: Int, count: Int, after: Int) {
+                if (text.isEmpty()) {
+                    binding.loginButton.isEnabled = false
+                    binding.registerButton.isEnabled = false
+                    return
+                }
+
+                if (text.length in 1..4) {
+                    usernameLabel.error = "username is incorrect"
+                    usernameLabel.isErrorEnabled = true
+                } else
+                    usernameLabel.isErrorEnabled = false
+
+                binding.loginButton.isEnabled = !usernameLabel.isErrorEnabled
+                binding.registerButton.isEnabled = !usernameLabel.isErrorEnabled
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
     }
 }
